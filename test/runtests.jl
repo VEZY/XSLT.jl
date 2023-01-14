cba5d3724b7d1f2565edd09b45bff6f49b83d272
Rémi Vezy <VEZY@users.noreply.github.com>
2023-01-14 21:02:17 +0100

    First real commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
 using XSLT
 using Test
 
+# Test files are from https://www.w3schools.com/xml, e.g.: https://www.w3schools.com/xml/cd_catalog.xml
+
 @testset "XSLT.jl" begin
-    # Write your tests here.
+
+    file = joinpath(dirname(@__FILE__), "files", "cd_catalog.xml")
+    xslt = joinpath(dirname(@__FILE__), "files", "cd_catalog.xsl")
+
+    xsltParseStylesheetFile(xslt)
+    xmlParseFile(file)
+
+    doc_xslt_apply(file, xslt, String[])
 end
+
+Ref(file)
+
+mktemp() do f, io
+    write(io, "test")
+    close(io)
+    println(f)
+    println(io)
+end
+
+touch("tmp.txt")
+io = open("tmp.txt")
+io
+close(io)
