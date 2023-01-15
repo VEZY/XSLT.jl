f011ce5f4ed3d07a6dea75891813e9683b9b10cf
Rémi Vezy <VEZY@users.noreply.github.com>
2023-01-15 09:44:44 +0100

    Move tests on multiple files

---
+++ test/test-parameters.jl
@@ -0,0 +1,7 @@
+xml_file = joinpath(dirname(@__FILE__), "files", "doc.xml")
+xslt_file = joinpath(dirname(@__FILE__), "files", "style.xml")
+
+@testset "xml_xslt parameters" begin
+    @test xml_xslt(xml_file, xslt_file) == "author: \nreviewer: \n"
+    @test xml_xslt(xml_file, xslt_file, (author="jeroen", reviewer="bert")) == "author: jeroen\nreviewer: bert\n"
+end
