e826aaa01b2ab6a7c56db479604b1a518f8974f8
Rémi Vezy <VEZY@users.noreply.github.com>
2023-01-18 16:01:41 +0100

    Update test-errors.jl
    Use it for julia>= v1.8 because error messages changed

--- test/test-errors.jl
+++ test/test-errors.jl
@@ -1,12 +1,17 @@
-xml_file = joinpath(dirname(@__FILE__), "files", "cd_catalog.xml")
-xslt_file = joinpath(dirname(@__FILE__), "files", "cd_catalog.xsl")
+# The following tests are only valid for Julia 1.8 and above
+# because the error messages are a bit different for Julia 1.7 and below
 
-@testset "error test: xml file" begin
-    Test.@test_throws "unknown error of libxml2 or libxslt" XSLT.read_xml("nonsense.xml")
-    Test.@test_throws "AssertionError: `x` must be an XML document file path" XSLT.xml_xslt("nonsense.xml", "anotheone.xsl")
-end
+if VERSION >= v"1.8"
+    xml_file = joinpath(dirname(@__FILE__), "files", "cd_catalog.xml")
+    xslt_file = joinpath(dirname(@__FILE__), "files", "cd_catalog.xsl")
 
-@testset "error test: xslt file" begin
-    Test.@test_throws "unknown error of libxml2 or libxslt" XSLT.read_stylesheet("nonsense.xml")
-    Test.@test_throws "AssertionError: File `xslt` doesn't exist or is not an XSL or XSLT stylesheet" XSLT.xml_xslt(xml_file, "nonsense.xml")
+    @testset "error test: xml file" begin
+        Test.@test_throws "unknown error of libxml2 or libxslt" XSLT.read_xml("nonsense.xml")
+        Test.@test_throws "AssertionError: `x` must be an XML document file path" XSLT.xml_xslt("nonsense.xml", "anotheone.xsl")
+    end
+
+    @testset "error test: xslt file" begin
+        Test.@test_throws "unknown error of libxml2 or libxslt" XSLT.read_stylesheet("nonsense.xml")
+        Test.@test_throws "AssertionError: File `xslt` doesn't exist or is not an XSL or XSLT stylesheet" XSLT.xml_xslt(xml_file, "nonsense.xml")
+    end
 end
